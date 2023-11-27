7fd4f7e0104500270b036ecd4776a86ffbd3b00a
Antoine Planche <antoine.planche@ericsson.com>
2023-11-26 20:35:41 -0500

    Implémentation de la méthode build_class + Jeu de données supplémentaire

--- src/ca/uqam/info/mgl7460/json_loader.py
+++ src/ca/uqam/info/mgl7460/json_loader.py
@@ -8,8 +8,7 @@ from ca.uqam.info.mgl7460.meta.relationship import Relationship
 
 class json_loader:
 
-    # 
-    # output_path est le répertoire dans lequel on va générer le
+    # Output_path est le répertoire dans lequel on va générer le
     # code des classes implicites dans le fichier de données
     # json (fourni dans une autre méthode)
     # 
@@ -20,7 +19,6 @@ class json_loader:
     # 
     # Ici, je me sers du répertoire dans lequel je vais générer le code
     # pour "calculer" le "package cible" des classes générées
-    # 
     def __init__(self, output_path: str):
         self.output_path = output_path
         # the package is derived from the output path. It is whatever
@@ -30,8 +28,8 @@ class json_loader:
         print("Package: " + self.class_package)
         self.classes = dict()
 
-    #
-    # cette méthode lit un fichier json contenu dans un 
+
+    # Cette méthode lit un fichier json contenu dans un 
     # fichier portant le nom 'file_name' et se trouvant
     # dans le répertoire input_path
     # 
@@ -42,23 +40,21 @@ class json_loader:
     # Donc, dans notre cas particulier, le nom de la classe
     # recine sera boutique, étant donné que le fichier 
     # source s'appelle boutique.json
-    #
     def read_data(self, input_path: str, file_name: str):
         self.input_path = input_path
         self.input_file_name = file_name
         
-        # compute root class name based on file name
+        # Compute root class name based on file name
         self.top_class_name = self.input_file_name.split(".")[0]
         
-        # open the file
+        # Open the file
         json_file = open(self.input_path + '/' + self.input_file_name,'r')
     
-        # load the file into a json object
+        # Load the file into a json object
         self.jsobjet = json.load(json_file)
 
 
-    # 
-    # this method creates a JSONClass object with name class_name based 
+    # This method creates a JSONClass object with name class_name based 
     # on the structure of the json_fragment passed as an argument
     # If the json_fragment represents an aggregate object, the method
     # will descend recursively creating the component objects, and
@@ -72,78 +68,52 @@ class json_loader:
     # 
     # Voir la méthode main(...) de cette classe pour un exemple de "premier appel"
     # de la méthode
-    # 
     def build_class(self, class_name: str, json_fragment: dict):
         print ("entering build_class ...\n class_name is: " + class_name + " and json fragment is: " + json_fragment.__str__())
 
-        # Créer un objet JSONClass en passant le nom de la classe cible et
-        # du package comme arguments au constructeur. Initialement,
-        # la structure de la classe est une "coquille vide".
-        # On va ajouter des attributs et des "Relationp"s' en fonction de la
-        # structure du fragment json
+        # Crée un objet JSONClass pour la classe actuelle
         current_class = JSONClass(class_name, self.class_package)
 
-        # Maintenant, on itère sur le fragment json, qui est un dictionnaire, dont
-        # la partie 'clé' indique un nom d' "attribut" (donnée simple, e.g. nombre ou chaine
-        # de caractères), ou un nom de relation, au cas où la valeur correspondante est elle même 
-        # une structure complexe
-            # 1. si la clé commence par "liste_", on comprend tout de suite que le champ correspondant
-            #    correspond à une association (Relationship) liant l'objet présent à une entité dont
-            #    le nom se trouve après "liste_""
-            #    Par exemple, si la 'clé' du fragment json est "liste_clients", on comprend que
-            #    la partie valeur est: a) une liste, et b) que les éléments de la liste sont de type
-            #    "client". NOTEZ ici que j'ai enlevé le dernier "s".
-            #       
-            #    DONC, un champ json portant le nom "liste_produits" (ou "liste_clients") veut dire
-            #    que nous avons une association de UN À PLUSIEURS entre <la classe qu'on est en train 
-            #    de générer> (ici, 'boutique'), et une entité qui s'appelle "produit" ("client")
-            # 
-            # 2. si la clé commence par "table_", on comprend que le champ correspondant correspond à
-            #    une association (Relationship) UN À PLUSIEURS, qui est INDEXÉE liant l'objet présent 
-            #    à une entité dont le nom se trouve après "table_", mais en enlevant le dernier "s".
-            #    Dans le fichier boutique.json, il y a UN cas où un champ à une clé commençant par
-            #    "table_": il s'agit du 'champ' "table_ligne_commandes", qui fait partie de la représentation
-            #    de commande. On en déduit que: on a une entité appelée "ligne_commande" (notez que j'ai 
-            #    retiré le dernier "s").
-            # 
-            #    Que veut dire ici le fait que la relation soit INDEXÉE (i.e. on a "table_" au lieu de 
-            #    "liste_"): cela veut dire que: a) la partie à 'valeur' du fragment json est un dictionnaire,
-            #    au lieu d'une liste (et donc, au niveau du code généré plus tard, la 'variable d'instance'
-            #    correspondente sera un "dict()" plutôt que "[]"). Cela va aussi influencer la façon dont
-            #    on va générer le code pour la classe plus tard: dans un cas (relation non indexée), on fait
-            #    "append(...)" sur une liste, dans l'autre, on insère une <clé, valeur> avec une expression
-            #    du type 'self.table_ligne_commande["TAB1"] = <ligne commande pour le produit avec id "TAB1">
-            # 
-            #    Quand on insère un objet dans une table (dict()), quel attribut de l'objet doit on utiliser
-            #    comme clé? La règle ici est que l'on cherche l'agttribut qui commence par "id_": c'est la
-            #    valeur de cet attribut qui va servir comme clé d'insertion.
-            #   
-            # 3. Si le nom du champ ne commence ni par "liste_" ni par "table_", alors c'est soit: a) un attribut
-            #    simple (sa valeur est un type élémentaire de python), b) soit un attribut représentant un objet
-            #    complexe, auquel cas on a une association, mais cette fois-ci de UN À UN
-            #
-            #    Prenons l'exemple d'un fragment de notre fichier de données, qui correspond aux données pour
-            #    une cliente: 
-            #    { "id": "CL1", "nom": "Tremblay", "prenom": "Sylvie", "adresse": "Saguenay", 
-            #    "liste_commandes": [ { "id": "COM1", "table_ligne_commandes": { "CHA1": {"id_produit": "CHA1","quantite": 1},
-            #                                                                 "TAB1": {"id_produit": "TAB1","quantite": 1}}},
-            #                       { # deuxième commande }]
-            #    }
-            #     
-            #    Parce qu'on a trouvé un champ portant le nom "liste_commandeS", on en déduit que: 1) on a une association
-            #    de UN À PLUSIEURS entre 'client' et une entité appelée 'commande' (sans le s), et 2) la valeur du
-            #    champ correspondant (ce qui vient après le ":") est une LISTE d'EXEMPLAIRES de 'commande'. On prend
-            #    l'un de ces exemplaires (par exemple, le premier de la liste) et on appelle récursivement
-            #    build_class(<une variable dont la valeur est 'commande'>, <l'exemplaire JSON en question> )
-            #
-            #    Rendu dans la construction de 'commande' elle-même, je vais rencontrer "table_ligne_commandeS". On
-            #    en débuit que: 1) on a un une association de UN À PLUSIEURS de 'commande' vers une entité appelée
-            #    'ligne_commande', 2) la valeur du champ json correspondant n'est PAS une liste de 'ligne_commande',
-            #    mais plutôt un dictionnaire avec <clé, valeur> où c'est la partie valeur qui est une 'ligne_commande',
-            #    et 3) c'est l'attribut "id_propduit" de 'ligne_commande' qui est utilisé comme clé d'insertion. 
-            #    comment je sais ça? en fait, je prends l'attribut (le champ json) de 'ligne_commande' qui a "id_"
-            #    comme préfixe (je suppose qu'il y en a un seul)
-            #    
+        # Itére sur le fragment json
+        for key, value in json_fragment.items():
+            if isinstance(value, list):
+                # Traitement d'une liste : relation ONE_TO_MANY non indexée
+                related_class_name = key[6:] if key.startswith("liste_") else key
+                if related_class_name.endswith('s'):
+                    related_class_name = related_class_name[:-1]  # Enleve le 's' final
+                relation_name = related_class_name # Nom de la relation
+                current_class.add_relationship(relation_name, related_class_name, Relationship.ONE_TO_MANY)
+
+                # Appel récursif pour chaque élément de la liste
+                for item in value:
+                    self.build_class(related_class_name, item)
+
+            elif isinstance(value, dict):
+                if key.startswith("table_"):
+                    # Traitement d'un dictionnaire : relation ONE_TO_MANY indexée ou attribut complexe
+                    if key.startswith("table_"):
+                        related_class_name = key[6:]
+                        if related_class_name.endswith('s'):
+                            related_class_name = related_class_name[:-1]
+                    relation_name = related_class_name  # Nom de la relation
+                    # Identifie le champ d'indexation en parcourant les clés de l'objet
+                    sample_item = next(iter(value.values()))
+                    index_field = next((k for k in sample_item.keys() if k.startswith('id')), 'id_' + related_class_name)
+                    current_class.add_relationship(relation_name, related_class_name, Relationship.ONE_TO_MANY, index_field)
+
+                    # Appel récursif pour chaque valeur du dictionnaire
+                    for item in value.values():
+                        self.build_class(related_class_name, item)
+                else:
+                    # Relation ONE_TO_ONE ou attribut complexe
+                    related_class_name = key  # Le nom de la classe liée est la clé elle-même
+                    relation_name = related_class_name  # Nom de la relation
+                    current_class.add_relationship(relation_name, related_class_name, Relationship.ONE_TO_ONE)
+                    self.build_class(related_class_name, value) # Appel récursif pour l'objet complexe
+            else:
+                # Attribut simple
+                current_class.add_attribute(key, type(value).__name__)
+
         print ('The current class is: \n'+current_class.__str__())
 
         #insert class in classes dictionary
@@ -153,9 +123,7 @@ class json_loader:
         return current_class
         
 
-    #
-    # the "main" program
-    # 
+    # The "main" program
     def main(data_directory:str, input_data_file_name: str, code_output_directory: str):
         # 1. create an instance of loader
         loader = json_loader(code_output_directory)
@@ -175,7 +143,8 @@ class json_loader:
         # 5. read json data and create corresponding python objects
         top_object = top_class.create_object(loader.jsobjet)
         print ("Top object: "+ top_object.__str__())
-    
+
+
 if __name__ == '__main__':
 
     data_directory = "./data"
